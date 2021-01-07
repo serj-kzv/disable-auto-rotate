8b2bcc0ecead86ad3c5ca77a3dd81b8972b878d0
null <null>
2021-01-07 23:06:03 +0300

    Merge remote-tracking branch 'origin/trying-to-block-css-rotation-but-it-needs-css-parser' into main
    # Conflicts:
    #	src/background/disableCSPForTheExt.js
    #	src/background/main.js
    #	src/content/disableLock.js
    #	src/content/disableMozLockOrientation.js
    #	src/content/main.js
    #	src/disableLock.js
    #	src/disableMozLockOrientation.js
    #	src/manifest.json
    #	src/web_accessible_resources/disableLock.js
    #	src/web_accessible_resources/disableMozLockOrientation.js
    #	webpack.config.js

--- src/content/main.js
+++ src/content/main.js
@@ -1,64 +1,3 @@
-
-// class ElementMutator {
-//     constructor() {
-//         this.cssObserverCfg = {attributeFilter: ['style']};
-//         this.bodyObserverCfg = {childList: true};
-//         this.cssObserver = new MutationObserver(mutations => {
-//             mutations.forEach(mutation => {
-//                 if (mutation.type === 'attributes') {
-//                     this.handleCSS(mutation);
-//                 }
-//             });
-//         });
-//         this.bodyObserver = new MutationObserver(mutations => {
-//             mutations.forEach(mutation => {
-//                 if (mutation.type === 'childList') {
-//                     console.debug('mutation.addedNodes', mutation.addedNodes);
-//                     for (const node of mutation.addedNodes) {
-//                         console.debug('childList mutation');
-//                         console.debug(node);
-//                         if (node instanceof HTMLBodyElement && node.parentNode === document.documentElement) {
-//                             this.startCSSObserver(node);
-//                             this.bodyObserver.disconnect();
-//                             break;
-//                         }
-//                     }
-//                 }
-//             });
-//         });
-//     }
-//
-//     start() {
-//         console.debug('ElementMutator starts');
-//         this.startCSSObserver(document.documentElement);
-//         if (document.body) {
-//             this.startCSSObserver(document.body);
-//         } else {
-//             this.startBodyObserver();
-//         }
-//     }
-//
-//     startBodyObserver() {
-//         this.bodyObserver.observe(document.documentElement, this.bodyObserverCfg);
-//     }
-//
-//     startCSSObserver(targetNode) {
-//         this.cssObserver.observe(targetNode, this.cssObserverCfg);
-//     }
-//
-//     handleCSS(mutation) {
-//         const style = mutation.target.style;
-//         const important = 'important';
-//         const transform = 'transform';
-//         const transformValue = 'initial';
-//
-//         if (style.getPropertyPriority(transform) !== important || transformValue !== style.getPropertyValue(transform)) {
-//             style.setProperty(transform, transformValue, important);
-//         }
-//     }
-//
-// }
-
 const addScriptFn = async path => {
     const script = document.createElement('script');
 
@@ -73,19 +12,7 @@ const addScriptFn = async path => {
 
     script.remove();
 };
-const addScriptInlineFn = async path => {
-    const script = document.createElement('script');
-
-    script.async = false;
-    const url = browser.runtime.getURL(path);
-    console.debug('code url', url);
-    script.textContent = await (await fetch(url)).text();
-    (document.head || document.documentElement).appendChild(script);
-
-    script.remove();
-};
 const main = async () => {
-    // new ElementMutator().start();
     [
         '/web_accessible_resources/disableLock.js',
         '/web_accessible_resources/disableMozLockOrientation.js'
